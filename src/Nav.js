ff7213afce83b35b853c1ab3af6c939b15566ee0
Mountain_Michelle <michaelchinemelu24@gmail.com>
2022-11-06 04:31:11 +0100

    my last commit

---
+++ src/Nav.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect} from 'react'
+import './Nav.css'
+
+  function Nav() {
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+          handleShow(true);
+        } else handleShow(false);
+      });
+      return () => { 
+        window.removeEventListener("scroll", () => {});
+      };
+    },  []);
+
+
+  return (
+    <div className= {`nav ${show && "nav_black"}`}>
+      <img 
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/archive/6/69/20220504140801%21Netflix_logo.svg"
+        alt="Netflix logo"
+      />
+
+      <img 
+        className="nav_avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"
+        alt="Netflix logo"
+      />
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = "a054e4a0c8a1fa835968efdf0bdd9d4b";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMoveis: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
